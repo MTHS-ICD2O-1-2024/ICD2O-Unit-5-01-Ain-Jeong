b3f132eab6ed265c7df56a400241c9e5e47208fb
Ain-Jeong <ain.jeong@stu.ocsb.ca>
2025-04-15 17:28:48 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Ain Jeong All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Ain Jeong
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+/**
+ * This function checks if the user has selected the random number from above
+ */
+
+function checkNumber() {
+  // input
+  const userNumber = parseFloat(
+    document.getElementById("user-number").value
+  )
+
+  // process
+  if (userNumber == randomNumber) {
+    // output
+    document.getElementById("answer").innerHTML = "You have guessed the correct number!"
+  }
+  if (userNumber != randomNumber) {
+    // output
+    document.getElementById("answer").innerHTML = "You have guessed the wrong number!"
+  }
+}
